9a5f756d4ce8a7098f69bc03c84e7288f81305d0
Chris Hawkins <chawkins2019@gmail.com>
2023-08-18 14:33:27 -0700

    added main rocket launch page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [rocketsLaunched, setRocketsLaunched] = useState<number>(0);
+
+  // Load the count from storage when the app loads
+  useEffect(() => {
+    const loadRocketsFromStorage = async () => {
+      const storedCount = await AsyncStorage.getItem('rocketsLaunched');
+      if (storedCount !== null) {
+        setRocketsLaunched(Number(storedCount));
+      }
+    };
+    loadRocketsFromStorage();
+  }, []);
+
+  const launchRocket = async () => {
+    const newCount = rocketsLaunched + 1;
+    setRocketsLaunched(newCount);
+    await AsyncStorage.setItem('rocketsLaunched', String(newCount));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Launch Rocket" onPress={launchRocket} />
+      <Text style={{ color: '#fff', marginTop: 20 }}>Rockets Launched: {rocketsLaunched}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
