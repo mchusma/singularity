1b137322a248504b7d40261622d40fb76c019a7d
Chris Hawkins <chawkins2019@gmail.com>
2023-08-18 15:25:57 -0700

    added tabs for action and log

--- App.tsx
+++ App.tsx
@@ -1,42 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ActionTab from './src/screens/ActionTab';
+import LogTab from './src/screens/LogTab';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [rocketsLaunched, setRocketsLaunched] = useState<number>(0);
-
-  // Load the count from storage when the app loads
-  useEffect(() => {
-    const loadRocketsFromStorage = async () => {
-      const storedCount = await AsyncStorage.getItem('rocketsLaunched');
-      if (storedCount !== null) {
-        setRocketsLaunched(Number(storedCount));
-      }
-    };
-    loadRocketsFromStorage();
-  }, []);
-
-  const launchRocket = async () => {
-    const newCount = rocketsLaunched + 1;
-    setRocketsLaunched(newCount);
-    await AsyncStorage.setItem('rocketsLaunched', String(newCount));
-  };
-
   return (
-    <View style={styles.container}>
-      <Button title="Launch Rocket" onPress={launchRocket} />
-      <Text style={{ color: '#fff', marginTop: 20 }}>Rockets Launched: {rocketsLaunched}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Action" component={ActionTab} />
+        <Tab.Screen name="Log" component={LogTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
