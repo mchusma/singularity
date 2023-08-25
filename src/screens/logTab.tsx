3a31c21a867c0564ab36a1a5f4123e6468b17f6a
Andrew Hawkins <andrewhawkins13@gmail.com>
2023-08-24 23:47:36 -0700

    Log Updates

--- src/screens/logTab.tsx
+++ src/screens/logTab.tsx
@@ -1,12 +1,68 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, Dimensions, StyleSheet, Button, TextInput } from 'react-native';
+
+const screenHeight = Dimensions.get('window').height;
 
 function LogTab() {
+  const [logs, setLogs] = useState([
+    "Mission Command Log",
+  ]);
+
+  const [userInput, setUserInput] = useState('');
+
+  const addNewLog = () => {
+    if (userInput.trim()) {
+      setLogs(prevLogs => [...prevLogs, userInput]);
+      setUserInput('');
+    }
+  };
+
   return (
-    <View>
-      <Text>Log Tab</Text>
+    <View style={styles.mainContainer}>
+      <ScrollView style={styles.logContainer}>
+        {logs.map((log, index) => (
+          <Text key={index} style={styles.logMessage}>{log}</Text>
+        ))}
+      </ScrollView>
+      <TextInput
+        style={styles.input}
+        value={userInput}
+        onChangeText={setUserInput}
+        placeholder="Write your log here..."
+        placeholderTextColor="gray"
+      />
+      <Button title="Add to Log" onPress={addNewLog} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'black'
+  },
+  logContainer: {
+    maxHeight: screenHeight / 3,
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: 'black'
+  },
+  logMessage: {
+    marginBottom: 5,
+    color: 'white'
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    color: 'white',
+    backgroundColor: 'black'
+  }
+});
 
 export default LogTab;
